faaf5a784e816b9c0ed25fe217e843cb7c561663
dontsellshort <michaelhly@gmail.com>
2019-01-15 01:09:40 -0600

    added dependencies and truffle config

---
+++ truffle.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+require("babel-register");
+require("babel-polyfill");
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const infura = network => () => {
+  return new HDWalletProvider(
+    process.env.RINKEBY_PRIVATE_KEY,
+    `https://${network}.infura.io/${process.env.INFURA_API_KEY}`
+  );
+};
+
+module.exports = {
+  mocha: {
+    useColors: true,
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 21
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: process.env.JSON_RPC_PORT || 8545,
+      gas: 8000000,
+      network_id: "*"
+    },
+    mainnet: {
+      provider: infura("mainnet"),
+      gas: 8000000,
+      network_id: 1
+    },
+    kovan: {
+      provider: infura("kovan"),
+      gas: 6500000,
+      network_id: 42
+    },
+    rinkeby: {
+      provider: infura("rinkeby"),
+      gas: 6500000,
+      network_id: 4
+    }
+  }
+};
