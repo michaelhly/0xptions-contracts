b90cde592aaa00096723ff2ac86c446ad6c97084
dontsellshort <michaelhly@gmail.com>
2019-01-17 17:37:15 -0600

    fixed get_veth so approval event triggers

--- scripts/get_veth.js
+++ scripts/get_veth.js
@@ -1,17 +1,18 @@
 const Web3 = require("web3");
 const web3 = new Web3(Web3.givenProvider);
-const BN = web3.utils.BN;
+
 const VeilEther = artifacts.require("VeilEther");
+const VeilEthAddr = require("../tokens/veilEther.json").address;
 const ZrxERC20Proxy = "0x3e809c563c15a295e832e37053798ddc8d6c8dab";
 
 const main = async () => {
-  const veth = await VeilEther.deployed();
-  await veth.depositAndApprove(ZrxERC20Proxy, 2 ** 256 - 1, {
+  const veth = await VeilEther.at(VeilEthAddr);
+  const max = 2 ** 53 - 1;
+  await veth.depositAndApprove(ZrxERC20Proxy, max.toString(), {
     from: process.env.WALLET,
     value: web3.utils.toWei("100", "milliether")
   });
   const balance = await veth.balanceOf(process.env.WALLET);
-
   console.log(`0.1 Veil Ether was sent to ${process.env.WALLET}`);
   console.log(
     `You now have ${web3.utils.fromWei(balance.toString())} Veil Ether`
