53815451aa218843677394b917dca414e94a4ac6
dontsellshort <michaelhly@gmail.com>
2019-01-19 01:27:23 -0600

    redployed contracts to rinkeby and updated scripts

--- scripts/create_market.js
+++ scripts/create_market.js
@@ -102,7 +102,7 @@ const main = async () => {
 
   try {
     var createMarket = await FactoryInstance.methods
-      .createOptionMarket(
+      .createOptionsMarket(
         AugurAddresses.Universe,
         new BN(500).toString(),
         marketParams.endTime,
--- scripts/get_veth.js
+++ scripts/get_veth.js
@@ -2,13 +2,27 @@ const Web3 = require("web3");
 const web3 = new Web3(Web3.givenProvider);
 
 const VeilEther = artifacts.require("VeilEther");
-const VeilEthAddr = require("../tokens/veilEther.json").address;
-const ZrxERC20Proxy = "0x3e809c563c15a295e832e37053798ddc8d6c8dab";
+
+const ZrxERC20Proxy = {
+  mainnet: {
+    address: "0x2240dab907db71e64d3e0dba4800c83b5c502d4e"
+  },
+  rinkeby: {
+    address: "0x2f5ae4f6106e89b4147651688a92256885c5f410"
+  },
+  kovan: {
+    address: "0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e"
+  },
+  development: {
+    address: "0x0000000000000000000000000000000000000000"
+  }
+};
 
 const main = async () => {
-  const veth = await VeilEther.at(VeilEthAddr);
+  console.log(network);
+  const veth = await VeilEther.at("0x9c610ad5e7aedc4cb56523277d1122e6110aa958");
   const max = 2 ** 53 - 1;
-  await veth.depositAndApprove(ZrxERC20Proxy, max.toString(), {
+  await veth.depositAndApprove(ZrxERC20Proxy.rinkeby.address, max.toString(), {
     from: process.env.WALLET,
     value: web3.utils.toWei("100", "milliether")
   });
@@ -20,7 +34,7 @@ const main = async () => {
 };
 
 module.exports = cb => {
-  main()
+  main(network)
     .then(res => {
       cb(null, res);
     })
